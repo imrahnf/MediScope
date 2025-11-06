656fae5018d9960b9dfa64fb3940a26c3ecbb424
imrahn <35425886+ImrahnF@users.noreply.github.com>
2025-11-06 13:54:50 -0500

    small modifications

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MediScope.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ README.md
@@ -0,0 +1,8 @@
+# MediScope
+
+---
+### Notes:
+- run MediScope:`https`
+  - http port: `5000`
+  - https port: `5001`
+  - _see `launchSettings.json` for_ `applicationUrl` _info_
