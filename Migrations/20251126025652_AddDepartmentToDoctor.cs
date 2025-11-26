b022dd6e91367a6405b4e6ee3085190e00b516a9
MaryamElhamidi <unicolmaryame@gmail.com>
2025-11-25 22:10:31 -0500

    Views testing

---
+++ Migrations/20251126025652_AddDepartmentToDoctor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediScope.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentToDoctor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "Doctors",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_DepartmentId",
+                table: "Doctors",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_Departments_DepartmentId",
+                table: "Doctors",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_Departments_DepartmentId",
+                table: "Doctors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Doctors_DepartmentId",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Doctors");
+        }
+    }
+}
