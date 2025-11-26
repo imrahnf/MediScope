56f3e02fa2a0e221fbe7c9d8d65293bd175c5a50
MaryamElhamidi <unicolmaryame@gmail.com>
2025-11-25 23:21:27 -0500

    AMAZING landing page explaining entire project. and showing the pre-populated accounts for testing.

--- Program.cs
+++ Program.cs
@@ -73,7 +73,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 using (var scope = app.Services.CreateScope())
 {
